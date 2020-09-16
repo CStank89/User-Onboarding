1b5245e34b3be0bd8675b0e6551ef89f082e03f8
CStank89 <C.Stank89@gmail.com>
2020-09-16 15:09:19 -0400

    have a basic form out working on validations

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+             
+            <input 
+            type='text'
+            placeholder='name'/>
+            <input 
+            type='text'
+            placeholder='email'/>
+            <input 
+            type='text'
+            placeholder='password'/>
+            <input
+            type='checkbox' />
+
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default Form
